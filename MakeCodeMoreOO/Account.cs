1af33396dd8b44a7a675e37da6ad22b3aba64222
DESKTOP-J76PCRA\Hoang <hoangnguyenle.hnlhut@gmail.com>
2023-11-29 10:50:08 +0700

    add some more comment from account

--- MakeCodeMoreOO/Account.cs
+++ MakeCodeMoreOO/Account.cs
@@ -12,11 +12,17 @@ namespace MakeCodeMoreOO
             State = new NotVerified(onUnfreeze);
         }
 
+        // #1 (interaction): Deposit was invoked on the state
+        // #2 (behavior): Result of State.Deposit is new State
+        // #5 (behavior): Deposit 10, Deposit 1 - Balance == 11
         public void Deposit(decimal amount)
         {
             State = State.Deposit(() => Balance += amount);
         }
 
+        // #3 (interaction): WithDraw was invoked on the state
+        // #4 (behavior): Result of State.WithDraw is new State
+        // #6 (behavior): Deposit 1, Verify, Withdraw 1 - Balance ==9
         public void WithDraw(decimal amount)
         {
             State = State.WithDraw(() => Balance -= amount);
