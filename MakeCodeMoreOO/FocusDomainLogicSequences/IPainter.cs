bcf03547b7755799bceb838a96b1c7f9121290ba
DESKTOP-J76PCRA\Hoang <hoangnguyenle.hnlhut@gmail.com>
2023-11-30 10:41:28 +0700

    Use LINQ to object to focus domain logic with sequences

---
+++ MakeCodeMoreOO/FocusDomainLogicSequences/IPainter.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace FocusDomainLogicSequences
+{
+    public interface IPainter
+    {
+        bool IsAvailable { get; }
+        TimeSpan EstimateTimeToPaint(double sqMeters);
+        double EstimateCompensation(double sqMeters);
+    }
+
+    public class PainterA : IPainter
+    {
+        public bool IsAvailable => true;
+
+        public double EstimateCompensation(double sqMeters)
+        {
+            return sqMeters * 2;
+        }
+
+        public TimeSpan EstimateTimeToPaint(double sqMeters)
+        {
+            return new TimeSpan(1, 1, 1);
+        }
+    }
+
+    public class PainterB : IPainter
+    {
+        public bool IsAvailable => true;
+
+        public double EstimateCompensation(double sqMeters)
+        {
+            return sqMeters * 3;
+        }
+
+        public TimeSpan EstimateTimeToPaint(double sqMeters)
+        {
+            return new TimeSpan(1, 1, 1);
+        }
+    }
+
+    public class PainterC : IPainter
+    {
+        public bool IsAvailable => true;
+
+        public double EstimateCompensation(double sqMeters)
+        {
+            return sqMeters * 4;
+        }
+
+        public TimeSpan EstimateTimeToPaint(double sqMeters)
+        {
+            return new TimeSpan(1, 1, 1);
+        }
+    }
+}
---
+++ MakeCodeMoreOO/FocusDomainLogicSequences/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace FocusDomainLogicSequences
+{
+    internal class Program
+    {
+        /// <summary>
+        /// find which painter is available and have cheapest price to do
+        /// </summary>
+        /// <param name="sqMeters"></param>
+        /// <param name="painters"></param>
+        /// <returns></returns>
+        private static IPainter FindCheapestPainter(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            return painters
+                    .Where(painter => painter.IsAvailable)
+                    .WithMinimum(painter => painter.EstimateCompensation(sqMeters));
+        }
+        static void Main(string[] args)
+        {
+            List<IPainter> list = new List<IPainter>()
+            {
+                new PainterA(),
+                new PainterB(),
+                new PainterC(),
+            };
+
+            Console.WriteLine("Get cheapest Painter from the list painter");
+            var result = FindCheapestPainter(2, list);
+            Console.WriteLine($"{nameof(result)} with cheapest price is {result.EstimateCompensation(2)}");
+        }
+    }
+
+    public static class EnumerableExtentions
+    {
+        public static T WithMinimum<T, TKey>(this IEnumerable<T> sequence, Func<T, TKey> criterion) where T : class
+                     where TKey : IComparable<TKey>
+        {
+            var result = sequence
+            .Select(obj => Tuple.Create(obj, criterion(obj)))
+            .Aggregate((Tuple<T, TKey>)null,
+                (best, cur) => best == null || cur.Item2.CompareTo(best.Item2) < 0 ? cur : best)
+            .Item1;
+
+            return result;
+        }
+            
+    }
+
+}
