0e6bc98db7e5eb79d0f357f882e6bb5f91142e36
DESKTOP-J76PCRA\Hoang <hoangnguyenle.hnlhut@gmail.com>
2023-12-07 11:56:44 +0700

    add code of 5UsingImmutableObjectsWhenPossible

---
+++ MakeCodeMoreOO/5UsingImmutableObjectsWhenPossible/MoneyAmount.cs
@@ -0,0 +1,41 @@
+﻿namespace _5UsingImmutableObjectsWhenPossible
+{
+    sealed class MoneyAmount : IEquatable<MoneyAmount>
+    {
+        public decimal Amount { get; }
+        public string CurrencySymbol { get; }
+
+        public MoneyAmount(decimal amount, string currencySymbol)
+        {
+            Amount = amount;
+            CurrencySymbol = currencySymbol;
+        }
+
+        public MoneyAmount Scale(decimal factor)
+        => new MoneyAmount(Amount * factor, CurrencySymbol);
+
+        //do not overload operator -> make code quite hard readable
+        //public static MoneyAmount operator *(MoneyAmount amount, decimal factor) => amount.Scale(factor);
+
+        public override bool Equals(object? obj)
+        => Equals(obj as MoneyAmount);
+
+        //when you override Equals then you should override GetHashCode
+        public bool Equals(MoneyAmount other)
+            => other != null && Amount == other.Amount && CurrencySymbol == other.CurrencySymbol;
+
+        public override int GetHashCode()
+            => Amount.GetHashCode() ^ CurrencySymbol.GetHashCode();
+
+        public static bool operator ==(MoneyAmount a, MoneyAmount b)
+            => (ReferenceEquals(a, null) && ReferenceEquals(b, null)) || (ReferenceEquals(a, null) & a.Equals(b));
+
+        public static bool operator !=(MoneyAmount a, MoneyAmount b)
+           =>  !( a == b);
+
+        public override string ToString()
+        => $"{Amount} {CurrencySymbol}";
+
+       
+    }
+}
---
+++ MakeCodeMoreOO/5UsingImmutableObjectsWhenPossible/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace _5UsingImmutableObjectsWhenPossible
+{
+    internal class Program
+    {
+        static bool IsHappyHour { get; set; }
+        static MoneyAmount Reserve(MoneyAmount cost)
+        {
+            decimal factor = 1;
+            MoneyAmount moneyAmount = cost;
+            if (IsHappyHour)
+            {
+                factor = .5M;
+            }
+            Console.WriteLine($"\n Reserving an item that costs reduced to {cost}");
+            return moneyAmount.Scale(factor);
+        }
+
+        
+        static void Buy(MoneyAmount wallet, MoneyAmount cost)
+        { 
+            bool enoughMoney = wallet.Amount >= cost.Amount;
+
+            var newMoneyAmount = Reserve(cost);
+
+            bool realEnoughMoney = wallet.Amount >= newMoneyAmount.Amount;
+
+            if(realEnoughMoney)
+            {
+                Console.WriteLine($"You will pay {newMoneyAmount} with your wallet {wallet}");
+            }
+            else
+            {
+                Console.WriteLine($"You cannot pay {newMoneyAmount} with your {wallet}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Buy(new MoneyAmount(12, "USD"),
+            //    new MoneyAmount(10, "USD"));
+
+            //IsHappyHour = true;
+
+            //Buy(new MoneyAmount(7, "USD"),
+            //    new MoneyAmount(10, "USD"));
+
+            //MoneyAmount x = new MoneyAmount(12, "USD");
+            //MoneyAmount y = new MoneyAmount(12, "USD");
+
+            //HashSet<MoneyAmount> set = new HashSet<MoneyAmount>();
+
+            //set.Add(x);
+
+            //if(set.Contains(y))
+            //{
+            //    Console.WriteLine("Cannot add repeated value");
+            //}
+            //else
+            //{
+            //    set.Add(y);
+            //}
+
+            //Console.WriteLine($"Count: {set.Count}");
+
+
+            MoneyAmount x = new MoneyAmount(12, "USD");
+            MoneyAmount y = new MoneyAmount(12, "USD");
+
+            if(x == y)
+            {
+                Console.WriteLine("Values are equals");
+            }
+            else
+            {
+                Console.WriteLine("Values are not equal");
+            }
+        }
+    }
+}
